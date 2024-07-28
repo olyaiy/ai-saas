a106e4ae68ef5b42cb3380a8bfcc69aa4b298326
olyaiy <alexolyaiy@gmail.com>
2024-07-27 22:01:14 -0700

    minor ui fix

--- chatpdf/src/app/page.tsx
+++ chatpdf/src/app/page.tsx
@@ -5,10 +5,13 @@ import Image from "next/image";
 import Link from "next/link";
 import {LogIn} from 'lucide-react'
 import FileUpload from "@/components/FileUpload";
+import { checkSubscription } from "@/lib/subscription";
+import SubscriptionButton from "@/components/SubscriptionButton";
 
 export default async function Home() {
   const {userId} = await auth()
   const isAuth = !!userId
+  const isPro = await checkSubscription()
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -22,6 +25,10 @@ export default async function Home() {
 
             {/* if the user is authenticated, show the button to go to chats */}
             {isAuth && <Button> Go To Chats</Button> }
+            <div className="ml-3">
+              <SubscriptionButton isPro={isPro}/>
+            </div>
+
           </div>
 
          <p className="max-w-xl mt-1 text-lg text-slate-600">
