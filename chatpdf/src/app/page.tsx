fc131dae285303563386fb905e823ec9a80c9897
olyaiy <alexolyaiy@gmail.com>
2024-07-27 22:07:00 -0700

    fix routing of homepage buttons

--- chatpdf/src/app/page.tsx
+++ chatpdf/src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
@@ -7,11 +8,24 @@ import {LogIn} from 'lucide-react'
 import FileUpload from "@/components/FileUpload";
 import { checkSubscription } from "@/lib/subscription";
 import SubscriptionButton from "@/components/SubscriptionButton";
+import { db } from "@/lib/db";
+import { eq } from "drizzle-orm";
+import { chats } from "@/lib/db/schema";
 
 export default async function Home() {
   const {userId} = await auth()
   const isAuth = !!userId
   const isPro = await checkSubscription()
+  let firstChat;
+  if (userId) {
+    firstChat = await db.select().from(chats).where(eq(chats.userId, userId))
+    if (firstChat) {
+      firstChat = firstChat[0]
+    }
+
+  }
+
+
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -24,7 +38,11 @@ export default async function Home() {
           <div className="flex mt-2">
 
             {/* if the user is authenticated, show the button to go to chats */}
-            {isAuth && <Button> Go To Chats</Button> }
+            {isAuth && firstChat && 
+            <Link href={`chat/${firstChat.id}`}>
+              <Button> Go To Chats</Button> 
+            </Link>
+            }
             <div className="ml-3">
               <SubscriptionButton isPro={isPro}/>
             </div>
